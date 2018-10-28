355eeb241c72d5ce2f3cdfef897a8062574068c5
bjb568 <bjb568@users.noreply.github.com>
2018-10-28 12:18:24 -0700

    Good practices

--- tests/testthat/test_gpdd.R
+++ tests/testthat/test_gpdd.R
@@ -8,7 +8,7 @@ test_that("download_gpdd() is equal to download_gpdd(\"data\")", {
   expect_equal(data[[1]][[2]], download_gpdd("data"))
 })
 dims <- c(10, 25, 6, 12, 9, 3, 24)
-names(dims) = names(indexes)
+names(dims) <- names(indexes)
 lapply(data, function(f) {
   test_that(paste(f[[1]], "has the correct dimensions"), {
     expect_equal(dim(f[[2]])[2], dims[[f[[1]]]])
