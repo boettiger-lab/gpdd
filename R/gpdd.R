70829e002fbc2ba9109a5e1918668b507172bd92
bjb568 <bjb568@users.noreply.github.com>
2018-11-30 09:23:16 -0800

    Several fixes (#15)
    * Codemetar
    
    * tidy style
    
    * Fix roxygen
    
    * Replace placeholder text

--- R/gpdd.R
+++ R/gpdd.R
@@ -1,16 +1,3 @@
-
-#' Download Data from GPDD.
-#'
-#' @author Zihao Li, \email{zihao0104@@berkeley.edu}
-#' @name download_gpdd
-#' @param dataset_name A string of the name of the dataset.
-#' @return The requested dataset in tibble format.
-#' @importFrom readr read_csv
-#' @export
-#' @examples
-#' download_gpdd('data')
-#' download_gpdd('timeperiod')
-
 indexes <- 233:239
 names(indexes) <- c(
   "data",
@@ -22,12 +9,25 @@ names(indexes) <- c(
   "location"
 )
 
+#' Download Data from GPDD.
+#'
+#' @author Zihao Li, \email{zihao0104@@berkeley.edu}
+#' @name download_gpdd
+#' @param dataset_name A string of the name of the dataset.
+#' @return The requested dataset in tibble format.
+#' @importFrom readr read_csv
+#' @export
+#' @examples
+#' download_gpdd("data")
+#' download_gpdd("timeperiod")
+
 download_gpdd <- function(dataset_name = names(indexes)) {
   dataset_name <- match.arg(dataset_name)
   url <- paste(
     "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/df35b.",
     indexes[[dataset_name]],
     ".1",
-    sep = "")
+    sep = ""
+  )
   readr::read_csv(url)
 }
